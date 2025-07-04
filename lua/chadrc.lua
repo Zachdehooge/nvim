49d380a16f1ec24c2184ac1632f7e729ca7f7baa
Zach <Zacharydehooge@gmail.com>
2025-07-04 10:50:07 -0400

    refactor

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -1,12 +1,12 @@
--- This file needs to have same structure as nvconfig.lua 
+-- This file needs to have same structure as nvconfig.lua
 -- https://github.com/NvChad/ui/blob/v2.5/lua/nvconfig.lua
 
 ---@type ChadrcConfig
 local M = {}
 M.base46 = {
 	theme = "bearded-arc",
-  -- theme = "onedark",
-  dashboard = true,
+	-- theme = "onedark",
+	dashboard = true,
 	-- hl_override = {
 	-- 	Comment = { italic = true },
 	-- 	["@comment"] = { italic = true },
--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,82 +1,93 @@
 -- load defaults i.e lua_lsp
 require("nvchad.configs.lspconfig").defaults()
 
-local lspconfig = require "lspconfig"
+local lspconfig = require("lspconfig")
 
 -- EXAMPLE
 local servers = { "html", "cssls" }
-local nvlsp = require "nvchad.configs.lspconfig"
-local util = require "lspconfig/util"
+local nvlsp = require("nvchad.configs.lspconfig")
+local util = require("lspconfig/util")
 
 -- lsps with default config
 for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    on_attach = nvlsp.on_attach,
-    on_init = nvlsp.on_init,
-    capabilities = nvlsp.capabilities,
-  }
+	lspconfig[lsp].setup({
+		on_attach = nvlsp.on_attach,
+		on_init = nvlsp.on_init,
+		capabilities = nvlsp.capabilities,
+	})
 end
 
 local tf_capb = vim.lsp.protocol.make_client_capabilities()
 tf_capb.textDocument.completion.completionItem.snippetSupport = true
 
 lspconfig.terraformls.setup({
-  on_attach = nvlsp.on_attach,
-  flags = { debounce_text_changes = 150 },
-  capabilities = tf_capb,
+	on_attach = nvlsp.on_attach,
+	flags = { debounce_text_changes = 150 },
+	capabilities = tf_capb,
 })
 
-lspconfig.gopls.setup{
-  on_attach = nvlsp.on_attach,
-  capabilities = nvlsp.capabilities,
-  cmd = {"gopls"},
-  filetypes = {"go", "gomod", "gowork", "gotmpl"},
-  root_dir = util.root_pattern("go.work", "go.mod", ".git"),
-  settings = {
-    gopls = {
-      completeUnimported = true,
-      usePlaceholders = true,
-      analyses = {
-        unusedparams = true,
-      },
-    },
-  },
+lspconfig.gopls.setup({
+	on_attach = nvlsp.on_attach,
+	capabilities = nvlsp.capabilities,
+	cmd = { "gopls" },
+	filetypes = { "go", "gomod", "gowork", "gotmpl" },
+	root_dir = util.root_pattern("go.work", "go.mod", ".git"),
+	settings = {
+		gopls = {
+			completeUnimported = true,
+			usePlaceholders = true,
+			analyses = {
+				unusedparams = true,
+			},
+		},
+	},
+})
 
-}
+lspconfig.pyright.setup({
+	settings = {
+		python = {
+			analysis = {
+				typeCheckingMode = "off", -- can be "strict", "basic", or "off"
+				autoSearchPaths = true,
+				useLibraryCodeForTypes = true,
+			},
+		},
+	},
+})
 
 -- In your lua/configs/lspconfig.lua or similar file
 local on_attach = function(client, bufnr)
-  -- Your existing on_attach code...
-  
-  -- Enable inlay hints if supported
-  if client.server_capabilities.inlayHintProvider then
-    vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
-  end
+	-- Your existing on_attach code...
+
+	-- Enable inlay hints if supported
+	if client.server_capabilities.inlayHintProvider then
+		vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
+	end
 end
 
 -- Or enable globally for all buffers
 vim.api.nvim_create_autocmd("LspAttach", {
-  callback = function(args)
-    local client = vim.lsp.get_client_by_id(args.data.client_id)
-    if client and client.server_capabilities.inlayHintProvider then
-      vim.lsp.inlay_hint.enable(true, { bufnr = args.buf })
-    end
-  end,
+	callback = function(args)
+		local client = vim.lsp.get_client_by_id(args.data.client_id)
+		if client and client.server_capabilities.inlayHintProvider then
+			vim.lsp.inlay_hint.enable(true, { bufnr = args.buf })
+		end
+	end,
 })
 
 -- Optional: Add a keybinding to toggle inlay hints
-vim.keymap.set('n', '<leader>ih', function()
-  vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
-end, { desc = 'Toggle inlay hints' })
+vim.keymap.set("n", "<leader>ih", function()
+	vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
+end, { desc = "Toggle inlay hints" })
 
-require('lspconfig').rust_analyzer.setup({
-  settings = {
-    ['rust-analyzer'] = {
-      inlayHints = {
-        enable = true,
-      },
-    },
-  },
+require("lspconfig").rust_analyzer.setup({
+	settings = {
+		["rust-analyzer"] = {
+			inlayHints = {
+				enable = true,
+			},
+		},
+	},
 })
 
 -- configuring single server, example: typescript
